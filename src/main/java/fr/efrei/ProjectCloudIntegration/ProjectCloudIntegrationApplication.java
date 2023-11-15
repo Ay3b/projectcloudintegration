1f4c08156c3c39d2ab10cb614f61c6c2cc8f312d
ay3bgit <ayyub.baiteche@efrei.net>
2023-11-15 11:32:07 +0100

    First commit

---
+++ src/main/java/fr/efrei/ProjectCloudIntegration/ProjectCloudIntegrationApplication.java
@@ -0,0 +1,13 @@
+package fr.efrei.ProjectCloudIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCloudIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCloudIntegrationApplication.class, args);
+	}
+
+}
