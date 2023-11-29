0041045df8f7c4013a44ff900348e20d2d1fb5ab
Ay3b <ayyub.baiteche@efrei.net>
2023-11-29 17:55:30 +0100

    Docker

--- src/test/java/fr/efrei/projectcloudintegration/web/rest/ItemResourceIT.java
+++ src/test/java/fr/efrei/projectcloudintegration/web/rest/ItemResourceIT.java
@@ -1,11 +1,15 @@
 package fr.efrei.projectcloudintegration.web.rest;
 
+import fr.efrei.projectcloudintegration.domain.Item;
 import fr.efrei.projectcloudintegration.repository.ItemRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @TestPropertySource(
@@ -19,6 +23,15 @@ public class ItemResourceIT {
     @Transactional
     void createItem() throws Exception {
         int databaseSizeBeforeCreate = itemRepository.findAll().size();
-        assertThat(databaseSizeBeforeCreate).isEqualTo(5);
+        assertThat(databaseSizeBeforeCreate).isEqualTo(0);
+
+        Item item = new Item();
+        //item.setId(6);
+        item.setName("Pierre");
+        item.setPrice(12.00);
+        itemRepository.save(item);
+
+        List<Item> itemList = itemRepository.findAll();
+        assertThat(itemList).hasSize(databaseSizeBeforeCreate +1);
     }
 }
